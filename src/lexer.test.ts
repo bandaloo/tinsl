081ba031f1a7be5234a144a4f83cc81f0fa0d34a
bandaloo <bandaloo@icloud.com>
2020-11-20 16:02:18 -0500

    more operators and more tests

--- src/lexer.test.ts
+++ src/lexer.test.ts
@@ -10,23 +10,20 @@ const types = (tokens: Token[]) => tokens.map((t) => t.type);
 /** returns values of all tokens */
 const values = (tokens: Token[]) => tokens.map((t) => t.value);
 
-/** helper function to return uniform list separated by whitespace */
-function uniform(s: string, n: number) {
-  return [...Array(n)]
-    .map(() => [s, "ws"])
+/** helper function to put "ws" between all elements in array */
+const separate = (arr: string[]) =>
+  arr
+    .map((s) => [s, "ws"])
     .flat()
-    .slice(0, n * 2 - 1);
-}
+    .slice(0, arr.length * 2 - 1);
 
+/** helper function to return uniform list separated by whitespace */
+const uniform = (s: string, n: number) => separate([...Array(n)].map(() => s));
+
+/** helper function to get array of all tokens from lexer */
 const tokens = (lexer: Lexer, str: string) => {
   lexer.reset(str);
-  const arr: Token[] = [];
-  while (true) {
-    const next = lexer.next();
-    if (next === undefined) break;
-    arr.push(next);
-  }
-  return arr;
+  return Array.from(lexer);
 };
 
 describe("numbers", () => {
@@ -73,5 +70,79 @@ describe("numbers", () => {
 
   it("lexes `.` as period, not number", () => {
     expect(types(tokens(lexer, "."))).to.deep.equal(["period"]);
+  });
+});
+
+describe("operators", () => {
+  it("lexes grouping operators", () => {
+    expect(types(tokens(lexer, "(){}[]"))).to.deep.equal([
+      "lparen",
+      "rparen",
+      "lbrace",
+      "rbrace",
+      "lbracket",
+      "rbracket",
+    ]);
+  });
+
+  it("lexes arithmetic operators", () => {
+    expect(types(tokens(lexer, "+ - * / %"))).to.deep.equal(
+      separate(["plus", "minus", "mult", "div", "modulo"])
+    );
+  });
+
+  it("lexes relational operators", () => {
+    expect(types(tokens(lexer, "> < >= <="))).to.deep.equal(
+      separate(["gt", "lt", "gte", "lte"])
+    );
+  });
+
+  it("lexes assignment operators", () => {
+    expect(types(tokens(lexer, "== !="))).to.deep.equal(
+      separate(["eq", "neq"])
+    );
+  });
+
+  it("lexes bitwise operators", () => {
+    expect(types(tokens(lexer, "& ^ | << >>"))).to.deep.equal(
+      separate(["band", "bxor", "bor", "blshift", "brshift"])
+    );
+  });
+
+  it("lexes logical operators", () => {
+    expect(types(tokens(lexer, "&& || !"))).to.deep.equal(
+      separate(["and", "or", "not"])
+    );
+  });
+
+  it("lexes assignment operators", () => {
+    expect(
+      types(tokens(lexer, "= += -= *= /= %= &= ^= |= <<= >>="))
+    ).to.deep.equal(
+      separate([
+        "assignment",
+        "assign_plus",
+        "assign_minus",
+        "assign_mult",
+        "assign_div",
+        "assign_modulo",
+        "assign_band",
+        "assign_bxor",
+        "assign_bor",
+        "assign_blshift",
+        "assign_brshift",
+      ])
+    );
+  });
+
+  it("lexes ternary operators", () => {
+    expect(types(tokens(lexer, "?:"))).to.deep.equal([
+      "question_mark",
+      "colon",
+    ]);
+  });
+
+  it("lexes increment and decrement operators", () => {
+    expect(types(tokens(lexer, "++--"))).to.deep.equal(["incr", "decr"]);
   });
 });
