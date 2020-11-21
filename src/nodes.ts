681ea04dda38810b239c5335143470cbd4ea5cd6
bandaloo <bandaloo@icloud.com>
2020-11-21 01:58:04 -0500

    working on interfaces for ast nodes

--- src/nodes.ts
+++ src/nodes.ts
@@ -3,28 +3,84 @@ import type { Token } from "moo";
 export const test = "test!";
 
 // TODO stricter types for operator string
-interface BaseExpr {
-  line: number;
-  col: number;
+
+export type Expr =
+  | BinaryExpr
+  | UnaryExpr
+  | FloatExpr
+  | IntExpr
+  | VecExpr
+  | FuncCallExpr;
+
+export interface Type {
+  id: "type";
+  type: Token | null;
 }
 
-interface BinaryExpr extends BaseExpr {
-  operator: string;
-  left: BaseExpr;
-  right: BaseExpr;
+export interface BinaryExpr {
+  id: "binaryexpr";
+  operator: Token;
+  left: Expr;
+  right: Expr;
 }
 
-interface UnaryExpr extends BaseExpr {
-  operator: string;
-  argument: BaseExpr;
+export interface UnaryExpr {
+  id: "unaryexpr";
+  operator: Token;
+  argument: Expr;
 }
 
-interface FloatExpr extends BaseExpr {
-  value: string;
+export interface FloatExpr {
+  id: "floatexpr";
+  value: Token;
 }
 
-interface IntExpr extends BaseExpr {
-  value: string;
+export interface IntExpr {
+  id: "intexpr";
+  value: Token;
 }
 
-interface VecExpr extends BaseExpr {}
+export interface VecExpr {
+  id: "vecexpr";
+  call: Token;
+}
+
+export interface FuncDecl {
+  id: "funcdecl";
+  identifier: Token;
+  returnType: Type;
+  paramTypes: Type[];
+  paramIdents: Token[];
+  body: Expr[];
+}
+
+export interface FuncCallExpr {
+  id: "funccallexpr";
+  identifier: Token;
+  arguments: Expr[];
+}
+
+export interface ProcDecl {
+  id: "procdecl";
+  identifier: Token;
+  paramTypes: Type[];
+  paramIdents: Token[];
+  body: Expr[];
+}
+
+export interface ProcCall {
+  id: "proccall";
+  identifier: Token;
+  arguments: Expr[];
+}
+
+export interface RenderOp {
+  id: "renderop";
+  inChannel: Token | null;
+  outChannel: Token;
+  operations: (Expr | ProcCall)[];
+}
+
+export interface Program {
+  body: (ProcDecl | FuncDecl | RenderOp)[];
+}
